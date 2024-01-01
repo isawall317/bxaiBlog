3ac324d60c338e872d6b48a35de0a97fefafddc9
isawall317 <isawall317@outlook.com>
2024-01-01 17:35:29 +0800

    syn
    改成豆哥AI读书

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "豆哥AI职场";
+export const SITE_TITLE = "豆哥AI读书";
 export const SITE_DESCRIPTION =
-  "欢迎来到豆哥AI职场！这里专注于AI赋能职场的话题，在这里你可以通过了解AI、学习AI和精通AI，实现职场发展，在未来开启超级个体、一人公司的新职场道路。";
+  "欢迎来到豆哥AI读书！";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "豆哥AI职场";
-export const KEYWORDS = "超级个体,一人公司,职场,AI,ChatGPT,ai,认知模型, 思维模型, AI思维, AI资源, AI认知, GPT, GPT资源, 提示词工程, 提示词生成, 提示词, 生成式模型, 生成式AI, 生成式AI模型, 生成式AI应用, 生成式AI技巧, 生成式AI案例, 生成式AI实战,"
+export const MY_NAME = "豆哥AI读书";
+export const KEYWORDS = "超级个体, 一人公司, 读书, AI, ChatGPT, ai, 知识管理, Obsidian, 认知模型, 思维模型, AI思维, AI资源, AI认知, GPT, GPT资源, 提示词工程, 提示词生成, 提示词, 生成式模型, 生成式AI, "
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
